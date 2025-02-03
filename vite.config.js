822a79df0074c536a7f4a880b9deb9c484680008
muhammadadmiral <admelar15@gmail.com>
2025-02-03 13:54:14 +0700

    Fix Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Pastikan base path sudah benar
+  server: {
+    strictPort: true,
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
 })
